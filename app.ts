8c2b30b11cc21a56b4409a8cbd417c3bcdef481e
LAVEEZAFAREED <162338181+LAVEEZAFAREED@users.noreply.github.com>
2024-04-22 07:15:57 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+while(condition)
+   {
+      let addTask = await inquirer.prompt(
+         [
+            {
+              name:'todo',
+              type: 'input',
+              message :'What do you want in your list'
+         },
+         {
+           name: 'addMore',
+           type: 'confirm',
+           message: 'What do you want to add more in your list',
+           default: 'false'
+         }
+     ]
+   );
+
+   todos.push(addTask.todo);
+   condition = addTask.addMore
+   console.log(todos)
+  }
